95055888e7d06c1d0c3080d185471d2c4937922c
chickenlittleish <snimer.25@acsamman.edu.jo>
2023-02-15 18:19:45 +0300

    Update imagefilter.py

--- imagefilter.py
+++ imagefilter.py
@@ -1,6 +1,7 @@
 # run pip install pillow to install
 from PIL import Image
 import math
+import sys
 
 def main():
     # Open image
@@ -21,14 +22,25 @@ def main():
     # place a pixel from the original image into the new image
     new_image.putpixel((50, 50), (r, g, b))
 
-    for x in range(width):
-        for y in range(height):
-            r,g,b = image.getpixel((x,y))
-            new_image.putpixel((x,y), (math.floor(r/2), math.floor(g/2), math.floor(b/2)))
-
-    # open the new image
-    new_image.show()
+    grayscale_question = input("Would you like to grayscale your photo?")
+    if grayscale_question[0] == "y":
+        grayscale_type = input("do you want to use method 1 or 2?")
+        if grayscale_type == "1" or "method 1":
+            for x in range(width):
+                for y in range(height):
+                    r,g,b = image.getpixel((x,y))
+                    new_image.putpixel((x,y), (math.floor(r+g+b/3)))
+                    new_image.show()
+        if grayscale_type == "2" or "method 2": 
+             
+    if grayscale_question[0] == "n":
+            sys.exit()
+gray=0.299red+0.587green+0.114
 
 
 if __name__ == "__main__":
     main()
+
+
+     # open the new image
+    new_image.show()
