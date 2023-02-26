1721734af373f094d6bc3d83ba6dbe96a3c70e40
chickenlittleish <snimer.25@acsamman.edu.jo>
2023-02-27 01:27:27 +0300

    Update Flip_Shrink_Hide.py

--- Flip_Shrink_Hide.py
+++ Flip_Shrink_Hide.py
@@ -23,11 +23,7 @@ def main():
     # place a pixel from the original image into the new image
     new_image.putpixel((50, 50), (r, g, b))
 
-    question_identity = input("How would you like to be addressed?\n")
-    print("Would you like to Flip, Shrink, or Hide your photo?")
-    print("Your options are: Flip/Shrink/Hide")
-    question = input("")
-    if question.lower() == "flip":
+    def flip():
         print("Dear " +question_identity+ ", would you like to flip your image vertically or horizontally?")
         print("Your options are: Horizontal/Vertical")
         flip_question = input("")
@@ -37,17 +33,19 @@ def main():
         if flip_question.lower() == "vertical":
             vertical = image.transpose(Image.FLIP_TOP_BOTTOM)
             vertical.show()
-    #if question.lower() == "shrink":
-        #shrink_question = int(input("by how much would you like to shrink your image\n"))
-        #shrunk_width = int(width // shrink_question)
-        #shrunk_height = int(height // shrink_question)
-        #shrunk_image = ((shrunk_width, shrunk_height))
-        #for new_width in range(1, shrunk_width):
-            #for new_height in range(1, shrunk_height):
-                #new_image[new_width, new_height] = image[50 + new_width, 50 + new_height]
-                #new_image.save('new.jpg',new_image)
-                #new_image.show()
-    if question.lower() == "hide":
+    
+    def shrink():
+        shrink_question = int(input("by how much would you like to shrink your image\n"))
+        shrunk_width = int(width // shrink_question)
+        shrunk_height = int(height // shrink_question)
+        shrunk_image = ((shrunk_width, shrunk_height))
+        for new_width in range(1, shrunk_width):
+            for new_height in range(1, shrunk_height):
+                new_image[new_width, new_height] = image[50 + new_width, 50 + new_height]
+                new_image.save('new.jpg',new_image)
+                new_image.show()
+
+    def hide(): 
         secret_message = input("what is your secret message?\n")
         split_message = ([*secret_message])
         for character in split_message:
@@ -64,7 +62,18 @@ def main():
                     hidden_b = int(b + filtered_binary_character)
                     new_image.putpixel((x,y), (hidden_r,hidden_g,hidden_b))
         new_image.show()
-        
-                           
+
+
+    question_identity = input("How would you like to be addressed?\n")
+    print("Would you like to Flip, Shrink, or Hide your photo?")
+    print("Your options are: Flip/Shrink/Hide")
+    question = input("")
+    if question.lower() == "flip":
+        flip()
+    if question.lower() == "shrink":
+        shrink()
+    if question.lower() == "hide":
+        hide()
+
 if __name__ == "__main__":
     main()
